14c0b340c788a89580de3a9b2753ac0a9d560fc6
Yuuta Segawa <yuuta@192.168.100.4>
2022-09-27 18:48:16 +0300

    feat: comment & slider

---
+++ src/js/components/swiper.js
@@ -0,0 +1,43 @@
+import Swiper, { Scrollbar } from 'swiper';
+
+Swiper.use([Scrollbar]);
+
+const swiper = new Swiper('.massage', {
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    dragSize: 115.19,
+    draggable: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  initialSlide: 0,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  simulateTouch: true,
+  slideToClickedSlide: true,
+
+  breakpoints: {
+    1600: {
+      slidesPerView: 4,
+      dragSize: 115.19,
+    },
+    1000: {
+      slidesPerView: 3,
+      scrollbar: {
+        dragSize: 75,
+      },
+    },
+    640: {
+      direction: 'vertical',
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    320: {
+      direction: 'vertical',
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+  }
+});
